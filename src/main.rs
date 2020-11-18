7e8715feeb0731bdc68fae747a2f44b70333209d
Ilya Maximov <mail@overlisted.net>
2020-11-18 23:54:15 +0100

    finally write the main

--- src/main.rs
+++ src/main.rs
@@ -65,10 +65,30 @@ mod nowmake {
     }
 }
 
+use std::{fs, process};
+
 fn main() {
     let mut requested_targets: Vec<String> = env::args().skip(1).collect();
     if requested_targets.len() == 0 {
         requested_targets.push(String::from(nowmake::DEFAULT_TARGET_NAME));
     }
+    
+    let targets_text = match fs::read_to_string(nowmake::FILE_NAME) {
+        Ok(content) => content,
+        Err(error) => {
+            println!("Can't open the targets file ({}): {:?}", nowmake::FILE_NAME, error);
+            process::exit(1);
+        }
+    };
+    
+    let targets = nowmake::Target::read_from(&targets_text);
+    
+    for target in targets {
+        if requested_targets.contains(&target.result) {
+            match target.now_make() {
+                Err(error) => println!("Failed: {}", error),
+                Ok(_) => println!("Made {} now.", target.result)
+            }
+        }
     }
 }
